3cc6910ca750e561b0c36b789e33f26d69bf7021
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2025-05-13 12:06:58 -0300

    Comando 1 para consultar datas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import psycopg2
 
 #connect to the db
 conn = psycopg2.connect(
-    dbname="teste",
+    dbname="Vendas",
     user="postgres",
     password="database123",
     host="localhost",
@@ -13,14 +13,43 @@ print("Conexao estabelecida com sucesso!")
 #cursor
 cur = conn.cursor()
 
-cur.execute("""CREATE TABLE teste (
-    id_teste serial primary key,
-    nome_teste varchar(100)
-);""")
+# 1. Utilizar os comandos relacionados com datas, tais como year, mouth, day e etc;
+print("\nPedidos feitos no mês de maio de 2025: ")
+cur.execute("""
+    SELECT * FROM pedidos
+    WHERE data_pedido >= DATE '2025-05-01' AND data_pedido < DATE '2025-05-31';
+""")
+
+pedidos_maio_2025 = cur.fetchall()
+
+for linha in pedidos_maio_2025:
+    print(linha)
+
+print("\nPedidos feitos no ano de 2025: ")
+cur.execute("""
+    SELECT * FROM PEDIDOS
+    WHERE EXTRACT(YEAR FROM data_pedido) = 2025;
+""")
+
+pedidos_2025 = cur.fetchall()
+for linha in pedidos_2025:
+    print(linha)
+
+print("\nPedidos feitos no dia 23 de abril: ")
+cur.execute("""
+    SELECT * FROM PEDIDOS
+    WHERE EXTRACT(MONTH FROM data_pedido) = 4;
+""")
+
+pedidos_23_abril = cur.fetchall()
+for linha in pedidos_23_abril:
+    print(linha)
+
+# 2. Realizar consultas com operadores relacionais e operadores lógicos;
 
 
 #commit the transiction
-conn.commit()
+# conn.commit()
 
 #close cursor
 cur.close()
