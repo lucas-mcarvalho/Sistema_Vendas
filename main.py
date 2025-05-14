66831881005f453fdbbcb12d050bcaec5d55cbbd
matheuspontes01 <matheus.pontes@mail.uft.edu.br>
2025-05-14 09:38:21 -0300

    Comandos 4,5 e 6 feitos (adicionei a biblioteca tabulate)

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import psycopg2
+from tabulate import tabulate
+
+def mostrar_resultado(cursor, descricao):
+    resultados = cur.fetchall()
+    colunas = [desc[0] for desc in cursor.description]
+    print(f"\n{descricao}")
+    print(tabulate(resultados, headers=colunas, tablefmt="grid"))
+
 
 #connect to the db
 conn = psycopg2.connect(
@@ -14,96 +22,139 @@ print("Conexao estabelecida com sucesso!")
 cur = conn.cursor()
 
 # 1. Utilizar os comandos relacionados com datas, tais como year, mouth, day e etc;
-print("\nPedidos feitos no mês de maio de 2025: ")
 cur.execute("""
     SELECT * FROM pedidos
     WHERE data_pedido < DATE '2025-04-30';
 """)
+mostrar_resultado(cur, "Pedidos feitos no mês de maio de 2025")
 
-pedidos_maio_2025 = cur.fetchall()
 
-for linha in pedidos_maio_2025:
-    print(linha)
-
-print("\nPedidos feitos no ano de 2025: ")
 cur.execute("""
     SELECT * FROM PEDIDOS
     WHERE EXTRACT(YEAR FROM data_pedido) = 2025;
 """)
+mostrar_resultado(cur, "Pedidos feitos no ano de 2025")
 
-pedidos_2025 = cur.fetchall()
-for linha in pedidos_2025:
-    print(linha)
 
-print("\nPedidos feitos no mes de abril: ")
 cur.execute("""
     SELECT * FROM PEDIDOS
     WHERE EXTRACT(MONTH FROM data_pedido) = 4;
 """)
-
-pedidos_abril = cur.fetchall()
-for linha in pedidos_abril:
-    print(linha)
+mostrar_resultado(cur, "Pedidos feitos no mes de abril")
 
 # 2. Realizar consultas com operadores relacionais e operadores lógicos;
-print("\n Pedidos com valores acima ou iguais a R$ 5500,00")
 cur.execute("""
     SELECT * FROM pagamento
     WHERE valor >= 2500.0;
 """)
+mostrar_resultado(cur, "Pedidos com valores acima ou iguais a R$ 5500,00")
 
-valores = cur.fetchall()
 
-for linha in valores:
-    print(linha)
-
-print("\nSelecionando cliente que possuem letra 'a' no nome:")
 cur.execute("""
     SELECT * FROM cliente
     where nome LIKE '%a%';
 """)
+mostrar_resultado(cur, "Selecionando cliente que possuem letra 'a' no nome")
 
-clientes = cur.fetchall()
-for linha in clientes:
-    print(linha)
 
-print("\nProdutos que possuem valores entre R$500 e R$1500")
 cur.execute("""
     SELECT * FROM produtos
     WHERE preco >= 500 AND preco <= 2000;
 """)
-
-produtos = cur.fetchall()
-for linha in produtos:
-    print(linha)
+mostrar_resultado(cur, "Produtos que possuem valores entre R$500 e R$1500")
 
 
 # 3. Fazer junções entre tabelas por chave estrangeira e primária
-print("\nConsultado tabelas de pedidos e itens_pedido de acordo com os ids: ")
 cur.execute("""
     SELECT pedidos.idcliente, itens_pedido.idpedido, pedidos.data_pedido, pedidos.total 
     FROM itens_pedido
     INNER JOIN pedidos ON itens_pedido.idpedido = pedidos.id_pedido
 ;""")
-
-pedidos_relacionados = cur.fetchall()
-
-for linha in pedidos_relacionados:
-    print(linha)
+mostrar_resultado(cur, "Consultado tabelas de pedidos e itens_pedido de acordo com os ids")
 
 
-print("\nConsultando os produtos utilizando right join:")
 cur.execute("""
         SELECT itens_pedido.idpedido, itens_pedido.idproduto, produtos.nome_produto, produtos.idfornecedor,
         itens_pedido.quantidade, produtos.preco 
         FROM itens_pedido
         RIGHT JOIN produtos ON itens_pedido.idproduto = produtos.id_produto
 ;""")
+mostrar_resultado(cur, "Consultando os produtos utilizando right join")
 
-p = cur.fetchall()
+# 4. Utilizar alias tanto na projeção quanto na declaração de tabelas;
+cur.execute("""
+        SELECT 
+        c.nome AS nome_cliente,
+        p.nome_produto AS nome_produto,
+        ip.quantidade AS qtd,
+        pg.metodo_de_pagamento AS metodo_pagamento,
+        pg.status_pagamento AS status_pagamento,
+        pg.valor AS valor_total
+    FROM 
+        Pedidos pd
+    JOIN 
+        Cliente c ON pd.idcliente = c.id_cliente
+    JOIN 
+        Itens_Pedido ip ON pd.id_pedido = ip.idpedido
+    JOIN 
+        Produtos p ON ip.idproduto = p.id_produto
+    JOIN 
+        Pagamento pg ON pd.id_pedido = pg.idpedido;
+""")
+mostrar_resultado(cur, "Listando pedidos com nome do cliente, nome do produto, quantidade, metodo de pagamento, status e valor total")
 
-for linha in p:
-    print(linha)
+
+# 5. Aplicar comandos ALL e DISTINCT e realizar agrupamentos GROUP BY
+cur.execute("""
+    SELECT DISTINCT metodo_de_pagamento
+    FROM pagamento;
+""")
+mostrar_resultado(cur, "Exibindo todos os metodos de pagamentos unicos")
+
+cur.execute("""
+    SELECT
+        status_pagamento,
+        COUNT(*) AS total_pedidos
+    FROM
+        pagamento
+    GROUP BY
+        status_pagamento; 
+""")
+mostrar_resultado(cur, "Quantidade de pedidos por status de pagamento")
+
+cur.execute("""
+    SELECT * 
+    FROM pagamento
+    WHERE valor > ALL (SELECT valor FROM pagamento WHERE metodo_de_pagamento = 'Pix');
+""")
+mostrar_resultado(cur,"Pagamentos com valor maior que todos os do método Pix")
+
+# 6. Fazer consultas com os comandos de conjuntos UNION, INTERSECTION, EXCEPT;
+print("\nUNION:")
+cur.execute("""
+    SELECT nome FROM CLIENTE
+    UNION
+    SELECT nome_fornecedor FROM fornecedor;
+""")
+mostrar_resultado(cur, "Cliente e Fornecedores (sem repeticao)")
+
+print("\nINTERSECTION: Nomes que aparecem como cliente e fornecedor")
+cur.execute("""
+    SELECT nome FROM cliente
+    INTERSECT
+    SELECT nome_fornecedor FROM fornecedor;
+""")
+mostrar_resultado(cur, "Nomes em comum (cliente e fornecedor)")
+
+print("\nEXCEPT:")
+cur.execute("""
+    SELECT nome FROM cliente
+    EXCEPT
+    SELECT nome_fornecedor FROM fornecedor;
+""")
+mostrar_resultado(cur, "Clientes que nao sao fornecedores")
+
+
 
 #commit the transiction
 # conn.commit()
