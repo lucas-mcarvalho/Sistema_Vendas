76656c97684594abe23f91a2c3e6f4c90c1efb08
Lucas Monteiro <lucasggtyyy358@gmail.com>
2025-05-21 18:51:17 -0300

    Testando deploy

--- Database commands.sql
+++ Database commands.sql
@@ -40,14 +40,14 @@ create table produtos(
 	foreign key(idfornecedor) references fornecedor(id_fornecedor)
 );
 
-create table itens_pedido (
-    id_itens_pedido serial primary key,
-    idpedido integer not null,
-    idproduto integer not null,
-    quantidade integer,
+create table itens_pedido(
+	id serial primary key,
+	idpedido integer not null,
+	idproduto integer not null,
+	quantidade integer,
 
-    foreign key (idpedido) references pedidos(id_pedido),
-    foreign key (idproduto) references produtos(id_produto)
+	foreign key (idpedido) references pedidos(id_pedido),
+	foreign key (idproduto) references produtos(id_produto)
 );
 
 create table pagamento(
@@ -63,20 +63,40 @@ create table pagamento(
 -- Inserção de valores nas tabelas
 
 -- Criando novo cliente
-insert into cliente(nome, cpf, telefone) values ('Marcelo', '12345678901', '11987654321')
+insert into cliente(nome, cpf, telefone) values ('Marcelo', '12345678901', '11987654321');
 
 -- Criando um fornecedor
 insert into fornecedor(nome_fornecedor, email, telefone_fornecedor)
-values ('Castor', 'castorcolchoes@gmail.com', '21998765432')
+values ('Castor', 'castorcolchoes@gmail.com', '21998765432');
 
 -- Criando um produto
 insert into produtos(idfornecedor, nome_produto, preco, estoque)
-values (1, 'Colchão Silver Star', '1099.99', 15)
+values (1, 'Colchão Silver Star', '1099.99', 15);
 
 -- Criando tipos para metodo_pagamento e status_pagamento
+create type metodo_pagamento as ENUM('Cartão de Crédito', 'PIX', 'Boleto', 'Dinheiro');
+create type status_pagamento_enum as ENUM('Pendente', 'Pago', 'Cancelado');
 
--- create type metodo_pagamento as ENUM('Cartão de Crédito', 'PIX', 'Boleto', 'Dinheiro')
--- create type status_pagamento_enum as ENUM('Pendente', 'Pago', 'Cancelado')
+-- Criando novo pedido
+insert into pedidos(idcliente, data_pedido, total) 
+values (1, '14/04/2025', 1099.99);
+
+-- Inserindo dado na tabela Itens_Pedido
+insert into itens_pedido (idpedido, idproduto, quantidade)
+values (1, 1, 1);
+
+-- Inserindo novo pagamento
+insert into pagamento (idpedido, valor, metodo_de_pagamento, status_pagamento)
+values (1, 1099.99, 'PIX', 'Pago');
+
+-- Tentando ler as tabelas com as informações necessárias de acordo com a relação inner join
+select pedidos.idcliente, itens_pedido.idpedido, pedidos.data_pedido, pedidos.total from itens_pedido
+inner join pedidos on itens_pedido.idpedido = pedidos.id_pedido;
+
+-- Tentando ler as tabelas com as informações necessárias de acordo com a relação right join
+select itens_pedido.idpedido, itens_pedido.idproduto, produtos.nome_produto, produtos.idfornecedor,
+itens_pedido.quantidade, produtos.preco from itens_pedido
+right join produtos on itens_pedido.idproduto = produtos.id_produto;
 
 
 
